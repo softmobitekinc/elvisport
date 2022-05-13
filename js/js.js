922fef1eaddf70005a14e1ce802bcf2fa927b35c
Elvis Darko <103620517+softmobitekinc@users.noreply.github.com>
2022-05-13 05:05:56 -0700

    Update js.js

--- js/js.js
+++ js/js.js
@@ -1,4 +1,23 @@
    
+document.onkeydown = function(e) {
+    if(e.keyCode == 123) {
+     return false;
+    }
+    if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)){
+     return false;
+    }
+    if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)){
+     return false;
+    }
+    if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)){
+     return false;
+    }
+
+    if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)){
+     return false;
+    }      
+ }
+  
 function toggleMobileMenu(menu) {
     menu.classList.toggle('open');
 }
